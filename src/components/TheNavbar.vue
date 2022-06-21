fbaabb7081aea8e64ae845aefaca30e0b3e9e3d2
fracabu <100199965+fracabu@users.noreply.github.com>
2022-06-22 00:16:07 +0200

    mod

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
 
-    <div class="container-nav d-flex justify-content-start">
-      <div class="flex-grow-1"><img src="/img/dc-logo.png" alt=""></div>
-      <ul class="nav">
-        <!-- <li class="nav-item flex-grow-1"><img src="/img/dc-logo.png" alt=""></li> -->
+    <div class="container-nav d-flex justify-content-start mx-5">
+      <div class="box-logo py-3"><img src="/img/dc-logo.png" alt="" class="logo"></div>
+      <ul class="nav align-items-center text-uppercase fw-bold">
         <li class="nav-item" v-for="(link, i) in navLinks" :key="i">
           <a class="nav-link" :href="link.href">{{ link.name }}</a>
         </li>
@@ -76,5 +75,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.box-logo {
+  flex-grow: 1;
+  margin-left: 200px;
+
+}
+
+.logo {
+  width: 80px;
 }
 </style>
