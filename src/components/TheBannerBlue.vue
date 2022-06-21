9cb48faf0cde2c168db8f71f79f8c93c7116a590
fracabu <100199965+fracabu@users.noreply.github.com>
2022-06-21 17:13:06 +0200

    mod bannerblue

--- src/components/TheBannerBlue.vue
+++ src/components/TheBannerBlue.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
-
+        <div class="row bg-primary">
+            <div class="col"><img src="/img/buy-comics-digital-comics.png" alt=""> testo</div>
+            <div class="col"><img src="/img/buy-comics-merchandise.png" alt="">testo</div>
+            <div class="col"><img src="/img/buy-comics-subscriptions.png" alt=""> testo</div>
+            <div class="col"><img src="/img/buy-comics-shop-locator.png" alt=""> testo</div>
+            <div class="col"> <img src="/img/buy-dc-power-visa.svg" alt=""> testo</div>
+        </div>
+        
     </div>
 </template>
 
