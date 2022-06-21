9bb9ecc65ed801b620f0d56524bd1c6247535ed4
fracabu <100199965+fracabu@users.noreply.github.com>
2022-06-21 15:47:49 +0200

    mod

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
-  <div>
-    
+  <div id="App">
+    <TheNavbar></TheNavbar>
+    <TheBannerBlue></TheBannerBlue>
+    <TheFooter></TheFooter>
+    <TheBannerSignUp></TheBannerSignUp>
+
+
   </div>
 </template>
 
 <script>
+import TheNavbar from "./components/TheNavbar.vue"
+import TheBannerBlue from "./components/TheBannerBlue.vue"
+import TheFooter from "./components/TheFooter.vue"
+import TheBannerSignUp from "./components/TheBannerSignUp.vue"
+
+
+
 export default {
-  data(){
-    return{
-      
-    }
-  }
+
+  name: "App",
+  components: { TheNavbar, TheBannerBlue, TheFooter, TheBannerSignUp },
 }
+
+
+
 </script>
 
 <style lang="scss">
